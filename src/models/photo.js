46cc7075afb8095c4de8c2fd6d1cd0f50986472e
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-08-11 15:27:51 +0700

    fix relation alias name

--- src/models/photo.js
+++ src/models/photo.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Photo.belongsTo(models.User, { as: 'user', foreignKey: 'contactId', foreignKeyConstraint: true })
     }
   };
   Photo.init({
