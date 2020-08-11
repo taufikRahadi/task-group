cb121f008698de119ce8e70f8028c5b191ad865b
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-08-11 15:46:50 +0700

    crud route photos

--- src/routes/photos.js
+++ src/routes/photos.js
@@ -4,5 +4,7 @@ const uploadCloudinary = require('../middleware/upload-cloudinary')
 
 router.get('/', PhotoController.index)
     .post('/', uploadCloudinary.single('photo'), PhotoController.store)
+    .patch('/:id', uploadCloudinary.single('photo', PhotoController.update))
+    .delete('/:id', PhotoController.delete)
 
 module.exports = router
