d3fbf2e818b1397aebfea3d05752b20caa6f5c5a
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-08-11 19:04:53 +0700

    route /photos using middleware

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const port = process.env.PORT | 3000
 const userRoute = require('./src/routes/users')
 const photoRoute = require('./src/routes/photos')
 const authRoute = require('./src/routes/auth')
+const auth = require('./src/middleware/auth')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/users', userRoute)
-app.use('/photos', photoRoute)
+app.use('/photos', auth, photoRoute)
 app.use('/auth', authRoute)
 
 app.listen(port, () => console.log('Listened on port ' + port))
